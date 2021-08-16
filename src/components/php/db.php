063fae5e24752e73492e402942280ab4a0bbf6c7
idodo27 <50807858+idodo27@users.noreply.github.com>
2021-08-16 12:47:20 +0300

    Update db.php

--- src/components/php/db.php
+++ src/components/php/db.php
@@ -5,10 +5,10 @@
  header("Access-Control-Allow-Origin: http://localhost:3000");
  
  
- define("HOST","fdb27.freehostingeu.com");
- define("USER","3848398_newdb");
- define("PASS","Lr)v?F{^5xs%Gx^V");
- define("DB","3848398_newdb");
+ define("HOST","name_of_server_host");
+ define("USER","Database_user");
+ define("PASS","Database_password");
+ define("DB","Database_user");
  
 
 $con = mysqli_connect(HOST,USER,PASS,DB);
